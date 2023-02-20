65b85d255e4c7f732a2d11ae6927d904a89229e8
Meguiito <ignaciojaramillo54@gmail.com>
2023-02-20 18:24:08 -0400

    changes

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request, make_response, send_file, request
+import io
+import flask
+from io import BytesIO
+import base64
+from flask_pymongo import PyMongo
+from flask_cors import CORS, cross_origin
+from pymongo import MongoClient
+import os
+from bson import ObjectId
+from dotenv import load_dotenv
+load_dotenv()
+
+
 app = Flask(__name__)
+client = MongoClient("mongodb+srv://"+os.environ["MONGO_USER"]+":"+os.environ["MONGO_PASSWORD"]+"@cluster0.sziulk8.mongodb.net/?retryWrites=true&w=majority")
+db = client.Facturas
+factras = db.facts
+db2=client.ImagenesFacturas
+imagees=db2.Images
+# Settings
+cors=CORS(app)
+
 
 
 @app.route("/")
