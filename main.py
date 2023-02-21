3ab4b9b8a040f9e272ff577731b160703cd062f0
Meguiito <91824782+Meguiito@users.noreply.github.com>
2023-02-21 09:19:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request, make_response, send_file, request
 import io
 import flask
 from io import BytesIO
+from gridfs.errors import NoFile
 import base64
 from flask_pymongo import PyMongo
 from flask_cors import CORS, cross_origin
